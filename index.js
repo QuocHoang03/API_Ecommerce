03b7dfacb3aeada4d35d41a117ec446fb36f360c
PHAMQUOCHOANG\quoch <quochoang22603>
2023-11-23 02:16:26 +0700

    session_2_day_23-11_create model product and route

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
-const authRouter = require("./routes/authRoute");
 const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
+
+// routes
+const authRouter = require("./routes/authRoute");
+const productRoute = require("./routes/productRoute");
 
 dbConnect();
 app.use(bodyParser.json());
@@ -14,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/api/user", authRouter);
+app.use("/api/product", productRoute);
 
 app.use(notFound);
 app.use(errorHandler);
